b4f05f0aa41f6c6ca1a80ac0f4a9d8654f7a1205
lolkof <lolkof@outlook.com>
2023-01-25 16:16:48 +0300

    fix

--- stalApi.py
+++ stalApi.py
@@ -12,12 +12,15 @@ class stalApi:
         self.session = aiohttp.ClientSession()
 
 
-    async def __request_get(self, endpoint, headers = None, apiUrl = None):
+    async def __request_get(self, endpoint, headers = None, apiUrl = None, method='get'):
         apiUrl = apiUrl if apiUrl else self.__api_url
         url = apiUrl+endpoint
-        print(url)
-        async with self.session.post(url, params = headers) as resp:
-            r = await resp.json()
+        if method == 'post':
+            async with self.session.post(url, data = headers) as resp:
+                r = await resp.json()
+        else:
+            async with self.session.get(url, headers = headers) as resp:
+                r = await resp.json()
         return r
 
 
@@ -27,8 +30,10 @@ class stalApi:
         return r
 
 
-    async def get_auction_history(self, region, item_id):
+    async def get_auction_history(self, item_id, region, additional = False, limit = 20, offset = 0):
         endpoint = f'{region}/auction/{item_id}/history'
+        params = f'?limit={limit}&additional={additional}&offset={offset}'
+        endpoint += params
         r = await self.__request_get(endpoint, self.authHeader)
         return r
 
@@ -40,8 +45,9 @@ class stalApi:
             "client_secret": self.client_secret, 
             "grant_type": "client_credentials"
             }
-        r = await self.__request_get(endpoint, headers, apiUrl = 'https://exbo.net/')
+        r = await self.__request_get(endpoint, headers, apiUrl = 'https://exbo.net/', method='post')
         self.appToken = r.get('access_token')
+        self.authHeader = {"Authorization": f"Bearer {self.appToken}"}
         return r
 
 
--- test.py
+++ test.py
@@ -2,14 +2,16 @@ from stalApi import stalApi
 from config import client_id, client_secret
 import asyncio
 
+print(client_id, client_secret)
 exemplar = stalApi(client_id = client_id, client_secret = client_secret)
 
 
 async def main():
     await exemplar.run()
     regions = await exemplar.get_regions()
-    print(exemplar.appToken)
-    print(regions)
+    # print(exemplar.appToken)
+    item_history = await exemplar.get_auction_history('y1q9', regions[0]['id'], True)
+    print(item_history)
     await exemplar.close()
 
 loop = asyncio.get_event_loop()
